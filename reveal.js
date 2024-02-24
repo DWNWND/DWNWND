c30eebeecf2c4e36e43bc73e4e5cc7b1389c0a29
Thea <theola51924@stud.noroff.no>
2024-02-24 14:50:37 +0700

    added infoboxes with content, and service content

---
+++ reveal.js
@@ -0,0 +1,22 @@
+// //https://alvarotrigo.com/blog/css-animations-scroll/
+
+function reveal() {
+  var reveals = document.querySelectorAll(".reveal");
+
+  for (var i = 0; i < reveals.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = reveals[i].getBoundingClientRect().top;
+    // var elementVisible = 150; juster dette tallet for når det skal reveales
+    var elementVisible = -50;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
+
+window.addEventListener("scroll", reveal);
+
+// To check the scroll position on page load
+reveal();
